5cdadf78d630b55fafe4078a21f7b368b67c1773
chimegerdene <chimgeebatuh@gmail.com>
2025-03-19 12:03:41 +0800

    home done

--- movie-app/src/app/page.tsx
+++ movie-app/src/app/page.tsx
@@ -2,13 +2,26 @@ import { Navigation } from "@/components/ui/Navigation";
 import { Scroll } from "@/components/ui/Scroll";
 import { MovieList } from "@/components/ui/MovieList";
 import Image from "next/image";
+import { Footer } from "@/components/ui/Footer";
 
 export default function Home() {
   return (
-    <div className="w-screen h-[1200px] flex flex-col min-h-screen gap-[32px]">
+    <div className="w-screen h-[1200px] flex flex-col min-h-screen gap-[32px] items-center">
       <Navigation />
       <Scroll />
-      <MovieList title="Upcoming" />
+      <MovieList
+        title="Upcoming"
+        api="https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=d67d8bebd0f4ff345f6505c99e9d0289"
+      />
+      <MovieList
+        title="Popular"
+        api="https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=d67d8bebd0f4ff345f6505c99e9d0289"
+      />
+      <MovieList
+        title="Top Rated"
+        api="https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=d67d8bebd0f4ff345f6505c99e9d0289"
+      />
+      <Footer />
     </div>
   );
 }
