7f9363e2397b2550ba078b1ee4706f0575e146a0
chimegerdene <chimgeebatuh@gmail.com>
2025-03-20 11:25:27 +0800

    desc cordinate

--- movie-app/src/components/ui/MovieCard.tsx
+++ movie-app/src/components/ui/MovieCard.tsx
@@ -9,8 +9,8 @@ interface MovieCardProps {
 
 export const MovieCard: React.FC<MovieCardProps> = ({ title, imdb, img }) => {
   return (
-    <button className="max-w-[260px] max-h-[440px] flex flex-col gap-1 justify-start items-start bg-[#F4F4F5] rounded-md hover:opacity-70">
-      <img src={img} className="w-full max-h-[340px] rounded-t-md" />
+    <button className="max-w-[260px] h-[440px] min-h-[440px] flex flex-col gap-1 justify-start items-start bg-[#F4F4F5] rounded-md hover:opacity-70">
+      <img src={img} className="w-[260px] h-[80%] rounded-t-md" />
 
       <div className="w-full flex flex-col justify-start items-start p-2">
         <div className="flex w-fit h-fit justify-start items-center gap-[4px]">
